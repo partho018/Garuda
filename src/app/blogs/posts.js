89cf05de8f740c17c5b4f0c970e274040e9f3ec1
partho018 <partho@gmail.com>
2026-07-11 14:43:32 +0600

    chore: remove deprecated firebase, migrate files and unused assets

--- src/app/blogs/posts.js
+++
@@ -1,9 +0,0 @@
-export const slugify = (text) => {
-    return text
-        .toLowerCase()
-        .replace(/[^a-z0-9]+/g, '-')
-        .replace(/(^-|-$)+/g, '');
-};
-
-// Deprecated: All posts have been successfully migrated to the Cloudflare D1 database
-export const SAMPLE_POSTS = [];
--- src/firebase.js
+++
@@ -1,3 +0,0 @@
-// Deprecated: Firebase configuration is no longer used.
-// The project has been fully migrated to Cloudflare D1.
-export const db = {};
